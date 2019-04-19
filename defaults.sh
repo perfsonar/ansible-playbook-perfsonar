cdd491648b1b79bb032b37a3572d604e2bc74e9b
Edward Paul Colone <epcjr@its-perfsonar-bastion.dsc.umich.edu>
2019-04-19 08:45:18 -0400

    added perfsonar subdirectory to group_vars

--- defaults.sh
+++ defaults.sh
@@ -1,17 +1,17 @@
 #/bin/sh
-mkdir -p inventory/group_vars/all
+mkdir -p inventory/group_vars/all/perfsonar
 mkdir -p inventory/host_vars/
 cp -i roles/ansible-role-perfsonar-archive/defaults/main.yml \
-  inventory/group_vars/all/ps_archive.yml
+  inventory/group_vars/all/perfsonar/ps_archive.yml
 cp -i roles/ansible-role-perfsonar-installer/defaults/main.yml \
-  inventory/group_vars/all/ps_installer.yml
+  inventory/group_vars/all/perfsonar/ps_installer.yml
 cp -i roles/ansible-role-perfsonar-maddash/defaults/main.yml \
-  inventory/group_vars/all/ps_maddash.yml
+  inventory/group_vars/all/perfsonar/ps_maddash.yml
 cp -i roles/ansible-role-perfsonar-psconfig-publisher/defaults/main.yml \
-  inventory/group_vars/all/ps_psconfig-publisher.yml
+  inventory/group_vars/all/perfsonar/ps_psconfig-publisher.yml
 cp -i roles/ansible-role-perfsonar-psconfig-web-admin/defaults/main.yml \
-  inventory/group_vars/all/ps_pwa.yml
+  inventory/group_vars/all/perfsonar/ps_pwa.yml
 cp -i roles/ansible-role-perfsonar-testpoint/defaults/main.yml \
-  inventory/group_vars/all/ps_testpoint.yml
+  inventory/group_vars/all/perfsonar/ps_testpoint.yml
 cp -i roles/ansible-role-perfsonar-toolkit/defaults/main.yml \
-  inventory/group_vars/all/ps_toolkit.yml
+  inventory/group_vars/all/perfsonar/ps_toolkit.yml
