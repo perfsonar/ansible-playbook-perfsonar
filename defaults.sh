a45a440322487f61a2b55b5c338b095d12f3e81c
Edward Paul Colone <epcjr@its-perfsonar-bastion.dsc.umich.edu>
2019-09-20 15:46:27 -0400

    added ability to make multiple inventory trees

--- defaults.sh
+++ defaults.sh
@@ -1,19 +1,55 @@
 #/bin/sh
-mkdir -p inventory/group_vars/all/perfsonar
-mkdir -p inventory/host_vars/
-cp -i roles/ansible-role-perfsonar-archive/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_archive.yml
-cp -i roles/ansible-role-perfsonar-installer/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_installer.yml
-cp -i roles/ansible-role-perfsonar-maddash/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_maddash.yml
-cp -i roles/ansible-role-perfsonar-psconfig-publisher/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_psconfig-publisher.yml
-cp -i roles/ansible-role-perfsonar-psconfig-web-admin/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_pwa.yml
-cp -i roles/ansible-role-perfsonar-testpoint/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_testpoint.yml
-cp -i roles/ansible-role-perfsonar-testpoint/defaults/lsregistration.yml \
-  inventory/
-cp -i roles/ansible-role-perfsonar-toolkit/defaults/main.yml \
-  inventory/group_vars/all/perfsonar/ps_toolkit.yml
+
+if [ "$1" == "" ]; then
+  directory="inventory"
+else
+  directory=$1
+fi
+#echo $directory/group_vars/all/perfsonar/ps_archive.yml
+#exit
+
+mkdir -p $directory/group_vars/all/perfsonar
+mkdir -p $directory/host_vars/
+
+wget -q -P $directory -nc \
+  https://raw.githubusercontent.com/perfsonar/ansible-playbook-perfsonar/master/inventory/hosts
+
+if ! [ -f $directory/lsregistration.yml ]; then
+  cp roles/ansible-role-perfsonar-testpoint/defaults/lsregistration.yml \
+    $directory/
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_archive.yml ]; then
+  cp roles/ansible-role-perfsonar-archive/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_archive.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_installer.yml ]; then
+  cp roles/ansible-role-perfsonar-installer/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_installer.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_maddash.yml ]; then
+  cp roles/ansible-role-perfsonar-maddash/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_maddash.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_psconfig-publisher.yml ]; then
+  cp roles/ansible-role-perfsonar-psconfig-publisher/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_psconfig-publisher.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_pwa.yml ]; then
+  cp roles/ansible-role-perfsonar-psconfig-web-admin/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_pwa.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_testpoint.yml ]; then
+  cp roles/ansible-role-perfsonar-testpoint/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_testpoint.yml
+fi
+
+if ! [ -f $directory/group_vars/all/perfsonar/ps_toolkit.yml ]; then
+  cp roles/ansible-role-perfsonar-toolkit/defaults/main.yml \
+    $directory/group_vars/all/perfsonar/ps_toolkit.yml
+fi
